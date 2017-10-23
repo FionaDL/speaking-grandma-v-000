28cb8145a3d14e50d3a28da737d6813b83e49171
Fiona Lapham <fionadlapham@gmail.com>
2017-10-23 03:50:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif "#{phrase}".upcase
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else "#{phrase}" == "I LOVE YOU GRANDMA!"
+  else phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
