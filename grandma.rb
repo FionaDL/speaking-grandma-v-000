237f9c798ffbd5cd275d01517098b1beaf263e43
Fiona Lapham <fionadlapham@gmail.com>
2017-10-23 03:49:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase)
   if phrase == phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif "#{phrase}".upcase
     "NO, NOT SINCE 1938!"
   else "#{phrase}" == "I LOVE YOU GRANDMA!"
