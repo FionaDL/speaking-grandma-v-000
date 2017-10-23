14363a0e05bae3486ab5bc6bec32f8cf6f066013
Fiona Lapham <fionadlapham@gmail.com>
2017-10-23 03:54:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase)
     if "#{phrase}" == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-  elsif "#{phrase}" == "#{phrase}".downcase
+  else "#{phrase}" == "#{phrase}".downcase
      "HUH?! SPEAK UP, SONNY!"
   end
 end
